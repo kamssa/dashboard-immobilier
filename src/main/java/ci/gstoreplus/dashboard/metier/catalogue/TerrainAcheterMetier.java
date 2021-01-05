79c3d5ff744e57f48f2332701ba6c6df375e458a
kamssa <kamssa0@gmail.com>
2021-01-05 04:46:40 +0000

    get terrain acheter par id personne

--- src/main/java/ci/gstoreplus/dashboard/metier/catalogue/TerrainAcheterMetier.java
+++ src/main/java/ci/gstoreplus/dashboard/metier/catalogue/TerrainAcheterMetier.java
@@ -4,5 +4,6 @@ import ci.gstoreplus.entity.catalogue.TerrainAcheter;
 import ci.gstoreplus.metier.Imetier;
 
 public interface TerrainAcheterMetier extends Imetier<TerrainAcheter, Long >{
+	TerrainAcheter findTerrainAcheteByIdPersonne(long id);
 
 }
