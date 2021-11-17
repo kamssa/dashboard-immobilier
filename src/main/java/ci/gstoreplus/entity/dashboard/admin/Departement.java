b2a0489277349025df3dc2617f898c8309a17c94
kamssa <kamssa0@gmail.com>
2021-11-17 15:29:14 +0000

    hash code

--- src/main/java/ci/gstoreplus/entity/dashboard/admin/Departement.java
+++ src/main/java/ci/gstoreplus/entity/dashboard/admin/Departement.java
@@ -1,5 +1,7 @@
 package ci.gstoreplus.entity.dashboard.admin;
 
+import java.util.Objects;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -63,6 +65,29 @@ public class Departement extends AbstractEntity{
 
 	public void setEntreprise(Entreprise entreprise) {
 		this.entreprise = entreprise;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(description, entreprise, libelle);
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departement other = (Departement) obj;
+		return Objects.equals(description, other.description) && Objects.equals(entreprise, other.entreprise)
+				&& Objects.equals(libelle, other.libelle);
 	}
 
 
